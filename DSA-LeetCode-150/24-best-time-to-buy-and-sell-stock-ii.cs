7318314b2667963ea25ddd6868169aa32e637d90
Mohammad Azam Shaikh <azam1200@gmail.com>
2026-09-18 09:18:40 +0530

    Add problem 24 Stock II C# solution

---
+++ DSA-LeetCode-150/24-best-time-to-buy-and-sell-stock-ii.cs
@@ -0,0 +1,10 @@
+public static class BestTimeToBuyAndSellStockII
+{
+    public static int MaxProfit(int[] prices)
+    {
+        int profit = 0;
+        for (int i = 1; i < prices.Length; i++)
+            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+        return profit;
+    }
+}
