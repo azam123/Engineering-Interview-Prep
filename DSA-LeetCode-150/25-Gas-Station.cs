7070f887ceb57faedb6346b8bcbecf61a48dd021
Mohammad Azam Shaikh <azam1200@gmail.com>
2026-09-18 09:38:00 +0530

    Add C# solution for LeetCode 25 - Gas Station

---
+++ DSA-LeetCode-150/25-Gas-Station.cs
@@ -0,0 +1,24 @@
+public static class GasStation
+{
+    public static int CanCompleteCircuit(int[] gas, int[] cost)
+    {
+        int total = 0;
+        int tank = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.Length; i++)
+        {
+            int diff = gas[i] - cost[i];
+            total += diff;
+            tank += diff;
+
+            if (tank < 0)
+            {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+
+        return total >= 0 ? start : -1;
+    }
+}
