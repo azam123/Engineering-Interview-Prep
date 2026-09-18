edbb95080aad6a215e910a95c6081235dc06517d
Mohammad Azam Shaikh <azam1200@gmail.com>
2026-09-18 09:51:36 +0530

    Add LeetCode 150 #26 Candy C# solution

---
+++ DSA-LeetCode-150/26-candy.cs
@@ -0,0 +1,19 @@
+public static class CandySolution
+{
+    public static int Calculate(int[] ratings)
+    {
+        int n = ratings.Length;
+        int[] candies = new int[n];
+        Array.Fill(candies, 1);
+
+        for (int i = 1; i < n; i++)
+            if (ratings[i] > ratings[i - 1]) candies[i] = candies[i - 1] + 1;
+
+        for (int i = n - 2; i >= 0; i--)
+            if (ratings[i] > ratings[i + 1]) candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
+
+        int total = 0;
+        foreach (int candy in candies) total += candy;
+        return total;
+    }
+}
