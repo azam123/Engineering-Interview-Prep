149027297e69aa18445b71f89b55480d78396267
Mohammad Azam Shaikh <azam1200@gmail.com>
2026-09-18 09:16:06 +0530

    Add problem 22 C# solution

---
+++ DSA-LeetCode-150/22-jump-game-ii.cs
@@ -0,0 +1,17 @@
+public static class JumpGameII
+{
+    public static int Jump(int[] nums)
+    {
+        int jumps = 0, currentEnd = 0, farthest = 0;
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            farthest = Math.Max(farthest, i + nums[i]);
+            if (i == currentEnd)
+            {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+        return jumps;
+    }
+}
