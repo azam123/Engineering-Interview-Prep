05a9f44ae619474ab3ae622307a179fe3ac038fd
Mohammad Azam Shaikh <azam1200@gmail.com>
2026-09-18 09:11:17 +0530

    Add 3Sum Closest C# solution

---
+++ DSA-LeetCode-150/15-three-sum-closest.cs
@@ -0,0 +1,32 @@
+using System;
+
+public static class ThreeSumClosest
+{
+    public static int Find(int[] nums, int target)
+    {
+        Array.Sort(nums);
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            int left = i + 1;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (Math.Abs(target - sum) < Math.Abs(target - closest))
+                    closest = sum;
+
+                if (sum == target)
+                    return sum;
+                if (sum < target)
+                    left++;
+                else
+                    right--;
+            }
+        }
+
+        return closest;
+    }
+}
